7aeae66f62706ef27c1148b8d6f026d77de3137c
rosesua318 <rosesua@duksung.ac.kr>
2023-03-04 01:20:56 +0900

    [#207] 프로그래머스 2016년 문제 완료

---
+++ programmers/level1/Year2016.java
@@ -0,0 +1,40 @@
+package programmers.level1;
+
+import java.util.Calendar;
+
+public class Year2016 {
+    class Solution {
+        public String solution(int a, int b) {
+            String answer = "";
+            Calendar cal = Calendar.getInstance();
+            cal.set(2016, a - 1, b);
+            int day = cal.get(Calendar.DAY_OF_WEEK);
+
+            switch(day) {
+                case 1:
+                    answer = "SUN";
+                    break;
+                case 2:
+                    answer = "MON";
+                    break;
+                case 3:
+                    answer = "TUE";
+                    break;
+                case 4:
+                    answer = "WED";
+                    break;
+                case 5:
+                    answer = "THU";
+                    break;
+                case 6:
+                    answer = "FRI";
+                    break;
+                case 7:
+                    answer = "SAT";
+                    break;
+            }
+
+            return answer;
+        }
+    }
+}
