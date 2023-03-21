ba396935621ab8588261afa3587182937cf9e47c
rosesua318 <rosesua@duksung.ac.kr>
2023-03-22 00:32:35 +0900

    [#274] 프로그래머스 덧칠하기 문제 완료

---
+++ programmers/level1/TopCoat.java
@@ -0,0 +1,20 @@
+package programmers.level1;
+
+public class TopCoat {
+    class Solution {
+        public int solution(int n, int m, int[] section) {
+            int answer = 0;
+            int max = 0;
+
+            for(int i = 0; i < section.length; i++) {
+                if(section[i] < max) {
+                    continue;
+                }
+                answer += 1;
+                max = section[i] + m;
+            }
+
+            return answer;
+        }
+    }
+}
