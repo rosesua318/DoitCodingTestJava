f1d33be74f39a149d8825e183afa62c4979983d2
rosesua318 <rosesua@duksung.ac.kr>
2023-03-26 00:03:16 +0900

    [#286] 프로그래머스 햄버거 만들기 문제 완료

---
+++ programmers/level1/Hamburger.java
@@ -0,0 +1,26 @@
+package programmers.level1;
+
+import java.util.*;
+
+public class Hamburger {
+    class Solution {
+        public int solution(int[] ingredient) {
+            int answer = 0;
+            Stack<Integer> stack = new Stack<>();
+            for(int n : ingredient) {
+                stack.push(n);
+                if(stack.size() >= 4) {
+                    int size = stack.size();
+                    if(stack.get(size - 1) == 1 && stack.get(size - 2) == 3 && stack.get(size - 3) == 2 && stack.get(size - 4) == 1) {
+                        answer++;
+                        stack.pop();
+                        stack.pop();
+                        stack.pop();
+                        stack.pop();
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+}
